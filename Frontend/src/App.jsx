01fd8ad742203b65daaa39866e174e73e3a6b688
AbeloByte <gobeztech20@gmail.com>
2024-10-05 03:03:13 -0400

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./App.css";
 import Signup from "./components/SignUp/SignUp";
 
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <>
